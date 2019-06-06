0968112ddb254d7d2720f9e4289e95520bb9bd6e
Black-Steel <wanghaxiang@gmail.com>
2019-06-07 01:32:48 +0800

    脚本未在kali linux上测试

---
+++ 3-RUN-Nvidia-Driver-Install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt-get -y install ocl-icd-libopencl1 nvidia-cuda-toolkit
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet "/GRUB_CMDLINE_LINUX_DEFAULT="quiet nvidia-drm.modeset=1"/g' /etc/default/grub
+sudo update-grub
+sed -i '$d' ~/.profile
+sudo reboot 
